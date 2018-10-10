bfc2f8d9cdc6d6ff9596e236197c4ab05599b56e
Ashlee <ashleefoureyes@gmail.com>
2018-10-10 19:13:00 +0100

    connect links and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_10_174742) do
+ActiveRecord::Schema.define(version: 2018_10_10_181146) do
 
   create_table "links", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2018_10_10_174742) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_links_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
